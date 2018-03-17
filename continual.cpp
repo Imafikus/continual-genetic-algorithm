b2d4ea05dbc2d40ed4508e0d34f18123b5ad3a95
imafikus <aleksatesicteske@gmail.com>
2018-03-17 17:42:15 +0100

    Initial commit

---
+++ continual.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <cstdlib>
+#include <ctime>
+#include <cmath>
+
+using namespace std;
+
+const int POPULATION_SIZE = 100;
+const double Epsilon = 0.1;
+
+int main()
+{
+    srand(time(NULL));
+
+}
+
+
